2f11290982397b2d252180b97dd9b41fd97a24b2
INIZIORUS <garanin203@bk.ru>
2020-03-30 12:56:38 +0400

    Вторая версия сайта. Список изменений: Доработана главная страница: добавление нового раздела "Последние работы", такие кнопки как, о нас, список ботов, кнопка вк, получили возможность переходить на другие страницы. Для возвращения домой используется картинка компании слева сверху.

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def main():
     return render_template('main.html', title="2DYES")
 
 
+@app.route('/about_us', methods=['GET', 'POST'])
+def about_usf():
+    return render_template('about_us.html', title="2DYES")
+
+
+@app.route('/list_bot', methods=['GET', 'POST'])
+def list_botf():
+    return render_template('list_bot.html', title="2DYES")
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
